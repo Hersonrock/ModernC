bcbd8799fd279a9140944e6145480fa3d79e079f
Hersonrock <hjpg20@gmail.com>
2024-06-06 08:29:00 -0500

    Completing test header

--- challenges/1_sorting/include/fileio.h
+++ challenges/1_sorting/include/fileio.h
@@ -20,9 +20,4 @@ int writeIn(double *inputArray, char *path);
 int randGen();
 int writeArray(char *path, double *outputArray);
 
-//---Debug Functions
-
-int testIncrementalOrder(char *path);
-int testDecreasingOrder(char *path);
-
 #endif // FILE_IO
--- challenges/1_sorting/src/main.c
+++ challenges/1_sorting/src/main.c
@@ -1,4 +1,5 @@
 #include "../include/fileio.h"
+#include "../include/test.h"
 
 int main(int argc, char *argv[argc + 1]){
        
